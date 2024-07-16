be3e7bc87e29653be45468df174f9c926e2cc095
iamdevcode <iamdevcode@gmail.com>
2024-07-16 12:40:03 -0400

    add color in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,19 @@
 export default {
   content: ['./src/**/*.{astro,jsx,}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#1f3e72',
+        secondary: '#ffffffc7',
+        gray: '#8c8b8b',
+        orange: '#ffa500',
+        blue: {
+          light: '#eeeeff',
+          default: '#4066ff'
+        },
+        black: '#131110'
+      }
+    }
   },
   plugins: []
 }
